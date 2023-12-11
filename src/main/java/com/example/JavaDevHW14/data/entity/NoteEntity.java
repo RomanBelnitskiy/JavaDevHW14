6fbfb02bb6a6b3cfa52f97e4f779545749deef19
Roman Belnitskiy <roman.belnitskiy@gmail.com>
2023-12-11 23:26:51 +0200

    completed

--- src/main/java/com/example/JavaDevHW14/data/entity/NoteEntity.java
+++ src/main/java/com/example/JavaDevHW14/data/entity/NoteEntity.java
@@ -1,5 +1,6 @@
 package com.example.JavaDevHW14.data.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,8 +10,14 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "notes")
 public class NoteEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "title", nullable = false, length = 150)
     private String title;
+    @Column(name = "content", nullable = false)
     private String content;
 }
